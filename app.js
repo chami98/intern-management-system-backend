f22ae00bcb5a62bd0a5ae094e80a10d1d3086d6d
Chamikara Mendis <chamikaram@99x.io>
2023-08-04 22:38:04 +0530

    Created API for create acoounts

--- app.js
+++ app.js
@@ -22,6 +22,40 @@ app.post("/login", (req, res) => {
   }
 });
 
+// In-memory array to store accounts
+const accounts = [];
+
+// Route for creating a new account
+app.post('/accounts', (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  // Validate data
+  if (!name || !email || !password || !role) {
+    return res.status(400).json({ message: 'Please provide all required fields.' });
+  }
+
+  // Check if the email is already registered
+  const existingAccount = accounts.find((account) => account.email === email);
+  if (existingAccount) {
+    return res.status(409).json({ message: 'An account with this email already exists.' });
+  }
+
+  // Create a new account object
+  const newAccount = {
+    id: accounts.length + 1,
+    name,
+    email,
+    password, // In a real-world scenario, you would hash the password before saving it.
+    role,
+  };
+
+  // Save the new account
+  accounts.push(newAccount);
+
+  // Return the new account details as the response
+  res.status(201).json(newAccount);
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
