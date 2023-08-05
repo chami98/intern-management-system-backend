34d5c650d994012b460f21b3f10bf8d9d6a63b9b
chami98 <chamikara98@gmail.com>
2023-08-05 18:53:12 +0530

    Created API for update intern profile status

--- app.js
+++ app.js
@@ -26,18 +26,22 @@ app.post("/login", (req, res) => {
 const accounts = [];
 
 // Route for creating a new account
-app.post('/register', (req, res) => {
+app.post("/register", (req, res) => {
   const { name, email, password, role } = req.body;
 
   // Validate data
   if (!name || !email || !password || !role) {
-    return res.status(400).json({ message: 'Please provide all required fields.' });
+    return res
+      .status(400)
+      .json({ message: "Please provide all required fields." });
   }
 
   // Check if the email is already registered
   const existingAccount = accounts.find((account) => account.email === email);
   if (existingAccount) {
-    return res.status(409).json({ message: 'An account with this email already exists.' });
+    return res
+      .status(409)
+      .json({ message: "An account with this email already exists." });
   }
 
   // Create a new account object
@@ -56,51 +60,63 @@ app.post('/register', (req, res) => {
   res.status(201).json(newAccount);
 });
 
-app.post('/invite', (req, res) => {
+app.post("/invite", (req, res) => {
   const { email, role } = req.body;
 
   // Validate data
   if (!email || !role) {
-    return res.status(400).json({ message: 'Please provide both email and role.' });
+    return res
+      .status(400)
+      .json({ message: "Please provide both email and role." });
   }
 
   // Check if the role is valid (Admin, Management, Intern, etc.)
-  const validRoles = ['Admin', 'Management', 'Intern', 'Evaluator', /* Add more roles as needed */];
+  const validRoles = [
+    "Admin",
+    "Management",
+    "Intern",
+    "Evaluator" /* Add more roles as needed */,
+  ];
   if (!validRoles.includes(role)) {
-    return res.status(400).json({ message: 'Invalid role provided.' });
+    return res.status(400).json({ message: "Invalid role provided." });
   }
 
   // Send invitation email
   sendInvitationEmail(email, role)
     .then(() => {
-      res.json({ message: 'Invitation sent successfully.' });
+      res.json({ message: "Invitation sent successfully." });
     })
     .catch((error) => {
-      console.error('Error sending invitation email:', error);
-      res.status(500).json({ message: 'Error sending invitation email.' });
+      console.error("Error sending invitation email:", error);
+      res.status(500).json({ message: "Error sending invitation email." });
     });
 });
 
 // Route for upgrading/downgrading permissions
-app.put('/api/users/:id/role', (req, res) => {
+app.put("/api/users/:id/role", (req, res) => {
   const { id } = req.params;
   const { role } = req.body;
 
   // Validate data
   if (!role) {
-    return res.status(400).json({ message: 'Please provide the new role.' });
+    return res.status(400).json({ message: "Please provide the new role." });
   }
 
   // Check if the role is valid (Admin, Mentor, Intern, etc.)
-  const validRoles = ['Admin', 'Mentor', 'Intern', 'Evaluator', /* Add more roles as needed */];
+  const validRoles = [
+    "Admin",
+    "Mentor",
+    "Intern",
+    "Evaluator" /* Add more roles as needed */,
+  ];
   if (!validRoles.includes(role)) {
-    return res.status(400).json({ message: 'Invalid role provided.' });
+    return res.status(400).json({ message: "Invalid role provided." });
   }
 
   // Find the user by ID in the database (Database link karanna)
   const user = users.find((user) => user.id === parseInt(id));
   if (!user) {
-    return res.status(404).json({ message: 'User not found.' });
+    return res.status(404).json({ message: "User not found." });
   }
 
   // Update the user's role
@@ -113,13 +129,17 @@ app.put('/api/users/:id/role', (req, res) => {
 const internProfiles = [];
 
 // Route to create an intern profile
-app.post('/api/interns', (req, res) => {
+app.post("/api/interns", (req, res) => {
   const data = req.body;
 
   // Check if a record with the same name already exists
-  const existingProfile = internProfiles.find(profile => profile.name === data.name);
+  const existingProfile = internProfiles.find(
+    (profile) => profile.name === data.name
+  );
   if (existingProfile) {
-    return res.status(409).json({ message: 'An intern profile with the same name already exists' });
+    return res
+      .status(409)
+      .json({ message: "An intern profile with the same name already exists" });
   }
 
   // Create a new intern profile
@@ -138,24 +158,31 @@ app.post('/api/interns', (req, res) => {
     assigned_team: data.assigned_team,
     mentor: data.mentor,
     cv_url: data.cv_url,
-    status: 'Pending' // Set initial status to Pending
+    status: "Pending", // Set initial status to Pending
   };
 
   // Save the intern profile in the temporary data storage (database)
   internProfiles.push(internProfile);
 
-  return res.status(201).json({ message: 'Intern profile created successfully', data: internProfile });
+  return res
+    .status(201)
+    .json({
+      message: "Intern profile created successfully",
+      data: internProfile,
+    });
 });
 
 // Route to update an intern profile
-app.put('/api/interns/:name', (req, res) => {
+app.put("/api/interns/:name", (req, res) => {
   const internName = req.params.name;
   const data = req.body;
 
   // Find the intern profile to be updated
-  const internProfile = internProfiles.find(profile => profile.name === internName);
+  const internProfile = internProfiles.find(
+    (profile) => profile.name === internName
+  );
   if (!internProfile) {
-    return res.status(404).json({ message: 'Intern profile not found' });
+    return res.status(404).json({ message: "Intern profile not found" });
   }
 
   // Update the intern profile information
@@ -174,11 +201,47 @@ app.put('/api/interns/:name', (req, res) => {
   internProfile.mentor = data.mentor;
   internProfile.cv_url = data.cv_url;
 
-  return res.json({ message: 'Intern profile updated successfully', data: internProfile });
+  return res.json({
+    message: "Intern profile updated successfully",
+    data: internProfile,
+  });
 });
 
+// Route to update intern profile status
+app.patch("/api/interns/:name/status", (req, res) => {
+  const internName = req.params.name;
+  const { status } = req.body;
 
+  // Find the intern profile to be updated
+  const internProfile = internProfiles.find(
+    (profile) => profile.name === internName
+  );
+  if (!internProfile) {
+    return res.status(404).json({ message: "Intern profile not found" });
+  }
 
+  // Check if the provided status is valid
+  const validStatuses = [
+    "Pending",
+    "Interview Scheduled",
+    "Interview Complete",
+    "Hired",
+    "Rejected",
+    "Internship Started",
+    "Internship Ended",
+  ];
+  if (!validStatuses.includes(status)) {
+    return res.status(400).json({ message: "Invalid status provided" });
+  }
+
+  // Update the intern profile status
+  internProfile.status = status;
+
+  return res.json({
+    message: "Intern profile status updated successfully",
+    data: internProfile,
+  });
+});
 
 // Start the server
 app.listen(port, () => {
